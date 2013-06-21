96cdf3c27b8b72e32b798cb6375b56677ee47a19
Alexander Standke <xanderstrike@gmail.com>
2013-06-21 14:09:04 -0700

    add uptime related information

--- server.rb
+++ server.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 
+def getgen
+  output = `uptime`.gsub(/\s+/m, ' ').split("up ").last.split(", ")
+  results = {uptime: output[0], since: output[1], users: output[2].to_i}
+end
+
 def getmem
   output = `free -m`.gsub(/\s+/m, ' ').strip.split(' ')
   results = {total: output[7], used: output[8], free: output[9], swaptotal: output[18], swapused: output[19], swapfree: output[20]}
@@ -16,14 +21,20 @@ def getdrives
   results = {name: output[0], total: output[1], used: output[2], free: output[3], percent: output[4]}
 end
 
+def getip
+  results = `curl -s http://checkip.dyndns.org | sed 's/[a-zA-Z/<> :]//g'`
+end
+
 set :port, 80
 set :bind, '0.0.0.0'
 
 get '/' do
+  gen = getgen
   cpu = getcpu
   mem = getmem
   hd = getdrives
   @output = []
+  @output << "<b>GENERAL</b><br>Uptime: #{gen[:uptime]}<br>Users: #{gen[:users]}<br>IP: #{getip}"
   @output << "<b>CPU</b><br>Used: #{cpu[:used]}%"
   @output << "<b>RAM</b><br>Used: #{mem[:used]}mb<br>Free:  #{mem[:free]}mb<br>Total: #{mem[:total]}mb"
   @output << "<b>SWAP</b><br>Used: #{mem[:swapused]}mb<br> Free: #{mem[:swapfree]}mb<br> Total: #{mem[:swaptotal]}mb"
